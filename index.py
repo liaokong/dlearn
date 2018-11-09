41211e2ddecc2695a350b76fa3b303cb30e6d353
liaojun <liaojun2009@gmail.com>
2018-11-09 22:55:33 +0800

    read data

--- index.py
+++ index.py
@@ -15,4 +15,10 @@ def read_data(lable_url, image_url): #读入训练数据
         magic, num = struct.unpack(">II", flb1.read(8)) # 读入标签文件头
         label = np.fromstring(flb1.read(), dtype=np.int8) # 读取标签内容
     with gzip.open(image_url) as fimg:
+        magic, num, rows, clos = struct.unpack(">IIII", fimg.read(16)) #读取图片文件头，rows 和cols
+        image = np.fromstring(fimg.read(), dtype=uint8)
+        image = image.reshape(len(label), 1, rows, cols) # 设置正确的数组格式
+        image = image.astype(np.float32)/255.0 # 归一化为0 - 1区间
+    return (label, image)
+    
 
